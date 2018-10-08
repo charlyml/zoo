59ddeb982e7d3300ef3e3ae3c0ea66d678b8945d
Carlos Maldonado <cmaldonadol@outlook.com>
2018-10-07 23:29:00 -0500

    Se agregan componentes para el panel de administracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,6 @@ import { Component, DoCheck, OnInit } from '@angular/core';
   	templateUrl: './app.component.html',
   	styleUrls: ['./app.component.css']
 })
-export class AppComponent implements DoCheck, OnInit {
+export class AppComponent {
   	title = 'NGZOO';
-  	emailContacto:string;
-
-  	ngOnInit() {
-  		this.emailContacto = localStorage.getItem("emailContacto");
-  	}
-
-  	ngDoCheck() {
-  		this.emailContacto = localStorage.getItem("emailContacto");
-  	}
-
-  	borrarEmail() {
-  		localStorage.removeItem("emailContacto");
-  		localStorage.clear();
-  		this.emailContacto = null;
-  	}
 }
